27ec6041032c6cc0ca61309d8104c18bc5965bef
Nathan Charles <38473514+N3thaniel10@users.noreply.github.com>
2023-06-12 15:53:47 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 // extract from chromium source code by @liuwayong
+export default Runner;
+
 (function () {
     'use strict';
     /**
